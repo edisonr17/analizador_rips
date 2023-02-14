a6c4658b5a5dfd1b5c9a0b33b6bcca86e2e8e037
Edison <edison.rave1517@gmail.com>
2023-02-13 20:29:40 -0500

    Rips

--- business/commons/readFile.ts
+++ business/commons/readFile.ts
@@ -1,25 +1,88 @@
 const fs = require('fs');
 const dataForge = require('data-forge-fs'); // For readFile/writeFile.
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
+export class ReadFiles {
+  constructor() {
 
-export class ReadFiles{
-    constructor(){
+  }
 
+  readFile = async (path: string) => {
+    return await fs.readFile(path, 'utf8');
+  }
+
+
+  getDataFromFileForge = async (path: string, columns: string[]) => {
+    if (fs.existsSync(path)) {
+      let data = await dataForge.readFile(path).parseCSV({ columnNames: columns });
+      return data.content.values.rows;
+    }
+    return [];
+  }
+
+  // Use fs.readFile() method to read the file
+
+
+  ///Método para leer datos de un archivo tipo xlsx
+  readXLSX = async (filepath: string, page: number, initialRow: number) => new Promise(async function (resolve, reject) {
+    console.log("____________________CREANDO ARCHIVOS DE EXCEL______________");
+
+    workbook["xlsx"].readFile(filepath).then(async function () {
+      let worksheet = workbook.getWorksheet(page);
+      let data: string[] = [];
+      await worksheet.eachRow({ includeEmpty: false }, function (row: any, rowNumber: number) {
+        if (rowNumber >= initialRow) {
+          data.push(row.values);
+        }
+      });
+      resolve(data);
+
+    }).catch(function (error: any) {
+      console.log(error);
+    });
+
+  });
+
+  escribirExcelGenerico =async (formatoOrigen: string, formatoSalida: string, datos: any, pagina = 1) =>  {
+    await  this.readExcel(formatoOrigen, formatoSalida, datos, pagina);
+    return true;
+  };
+
+
+
+
+
+
+  //let motorBDarchivo = file.split("_")[0];
+
+  readExcel = async (formatoOrigen: string, formatoSalida: string, datos: any, pagina: number) => {
+
+
+    console.log(formatoOrigen, formatoSalida, pagina)
+    //console.log(regimenesEncontrados)
+
+    //You can use the absolute path also
+    let filepath = formatoOrigen;
+    let workbook =await new Excel.Workbook();
+    await workbook.xlsx.readFile(filepath);
+  
+    //You can use the index number also eg. 0 for selecting the sheet
+
+    let worksheet = await workbook.getWorksheet("pag1");
+   
+    //console.log(formatoSalida, datos);
+    for (const element of datos) {
+      await worksheet.addRow(element).commit();
     }
 
-    readFile =async (path:string)=>{
-      return await fs.readFile(path, 'utf8');
-    } 
-    
 
-    getDataFromFileForge=async(path:string, columns:string[])=>{
-        if(fs.existsSync(path)){
-           let data = await dataForge.readFile(path).parseCSV({ columnNames: columns });
-           return data.content.values.rows;
-        }
-        return [];
-    }   
-  
-    // Use fs.readFile() method to read the file
-   
-      
+    //console.log(tiposCitas);
+
+
+    await workbook.xlsx.writeFile(formatoSalida);
+    return true;
+  }
+
 }
+
+
--- business/controllers/RipsController.ts
+++ business/controllers/RipsController.ts
@@ -1,33 +1,100 @@
 
-import { ReadFiles } from "./../commons/readFile"; 
+import { ReadFiles } from "./../commons/readFile";
 const alasql = require('alasql');
 
 export class RipsController {
     private readFiles;
-    constructor(){
+    constructor() {
         this.readFiles = new ReadFiles();
     }
-    objRips:any = {
-     
+    objRips: any = {
+
     };
     readRips = async () => {
 
         let columns = [];
-        this.objRips.AC = await this.readFiles.getDataFromFileForge('./files/rips/AC005312.txt', []);
-        this.objRips.AF = await this.readFiles.getDataFromFileForge('./files/rips/AF005312.txt', []);
-        this.objRips.AH = await this.readFiles.getDataFromFileForge('./files/rips/AH005312.txt', []);
-        this.objRips.AM = await this.readFiles.getDataFromFileForge('./files/rips/AM005312.txt', []);
-        this.objRips.AN = await this.readFiles.getDataFromFileForge('./files/rips/AN005312.txt', []);
-        this.objRips.AP = await this.readFiles.getDataFromFileForge('./files/rips/AP005312.txt', []);
-        this.objRips.AT = await this.readFiles.getDataFromFileForge('./files/rips/AT005312.txt', []);
-        this.objRips.AU = await this.readFiles.getDataFromFileForge('./files/rips/AU005312.txt', []);
-        this.objRips.CT = await this.readFiles.getDataFromFileForge('./files/rips/CT005312.txt', []);
-        this.objRips.US = await this.readFiles.getDataFromFileForge('./files/rips/US005312.txt', []);
+        this.objRips.AC = await this.readFiles.getDataFromFileForge('./files/rips/AC.txt', []);
+        this.objRips.AF = await this.readFiles.getDataFromFileForge('./files/rips/AF.txt', []);
+        this.objRips.AH = await this.readFiles.getDataFromFileForge('./files/rips/AH.txt', []);
+        this.objRips.AM = await this.readFiles.getDataFromFileForge('./files/rips/AM.txt', []);
+        this.objRips.AN = await this.readFiles.getDataFromFileForge('./files/rips/AN.txt', []);
+        this.objRips.AP = await this.readFiles.getDataFromFileForge('./files/rips/AP.txt', []);
+        this.objRips.AT = await this.readFiles.getDataFromFileForge('./files/rips/AT.txt', []);
+        this.objRips.AU = await this.readFiles.getDataFromFileForge('./files/rips/AU.txt', []);
+        this.objRips.CT = await this.readFiles.getDataFromFileForge('./files/rips/CT.txt', []);
+        this.objRips.US = await this.readFiles.getDataFromFileForge('./files/rips/US.txt', []);
+        this.objRips.config = await this.readFiles.readXLSX('./files/configFiles/HOSPITAL.xlsx',1,2);
 
-        var res = await alasql("SELECT [6], count([6]) FROM ? group by [6] ",[this.objRips.AC]);
+        let result:any = {};
+        result.AC_RESULT = await alasql(this.getQueryByFile("AC", this.getIndexFromFile("AC").cups), [this.objRips.AC]);
+        result.AM_RESULT = await alasql(this.getQueryByFile("AM", this.getIndexFromFile("AM").med), [this.objRips.AM]);
+        result.AP_RESULT = await alasql(this.getQueryByFile("AP", this.getIndexFromFile("AP").cups), [this.objRips.AM]);
+        result.AT_RESULT = await alasql(this.getQueryByFile("AT", this.getIndexFromFile("AT").cups), [this.objRips.AM]);
 
 
-        console.log(res);
+       
+        /*await this.readFiles.escribirExcelGenerico('./files/configFiles/HOSPITAL.xlsx','./results/SALIDA.xlsx', result.AC_RESULT, 2 );
+        await this.readFiles.escribirExcelGenerico('./files/configFiles/HOSPITAL.xlsx','./results/SALIDA2.xlsx', result.AM_RESULT, 3 );
+        await this.readFiles.escribirExcelGenerico('./files/configFiles/HOSPITAL.xlsx','./results/SALIDA.xlsx', result.AP_RESULT, 4 );
+        await this.readFiles.escribirExcelGenerico('./results/SALIDA.xlsx','./results/SALIDA.xlsx', result.AT_RESULT, 5 );*/
+        await this.readFiles.escribirExcelGenerico('./files/configFiles/HOSPITAL.xlsx','./results/SALIDA.xlsx', this.objRips.config, 6 );
+        console.log("terminado")
 
+    }
+
+
+
+    getQueryByFile = (type: string, indMov: string) => {
+        if (type == "AC") {
+            return `SELECT ac.[${indMov}] as codigo, count(ac.[${indMov}]) as conteo 
+            FROM ? ac group by [${indMov}]`;
+        }
+
+
+        if (type == "AM") {
+            return `SELECT ac.[${indMov}] as codigo, count(ac.[${indMov}]) as conteo
+            FROM ? ac group by [${indMov}]`;
+        }
+
+        if (type == "AP") {
+            return `SELECT ac.[${indMov}] as codigo, count(ac.[${indMov}]) as conteo
+            FROM ? ac group by [${indMov}]`;
+        }
+
+
+        if (type == "AT") {
+            return `SELECT ac.[${indMov}] as codigo, count(ac.[${indMov}]) as conteo
+            FROM ? ac group by [${indMov}]`;
+        }
+    }
+
+
+
+
+    getIndexFromFile = (type: string): any => {
+        if (type == "AC") {
+            return {
+                "cups": 6
+            }
+        }
+
+        if (type == "AM") {
+            return {
+                "med": 5
+            }
+        }
+
+        if (type == "AP") {
+            return {
+                "cups": 7
+            }
+        }
+
+
+        if (type == "AT") {
+            return {
+                "cups": 3
+            }
+        }
     }
 }
